2854396a492763a6f839487ffec355fc679fb6b3
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-24 08:18:36 +0000

    Hide the back button on home screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { IoIosArrowBack } from 'react-icons/io';
 import { HiMicrophone } from 'react-icons/hi';
 import { AiOutlineSetting } from 'react-icons/ai';
 import '../styles/navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const { inDetail } = useSelector((store) => store.continent);
@@ -15,7 +16,7 @@ const Navbar = () => {
   return (
     <nav className="nav-container">
       <ul className="nav-ul">
-        <li className="nav-li"><IoIosArrowBack /></li>
+        {inDetail && <li className="nav-li"><Link to="/"><IoIosArrowBack /></Link></li>}
         <li className="nav-li">covid stats</li>
         <li className="nav-li mic-and-setting">
           <div className="mic"><HiMicrophone /></div>
